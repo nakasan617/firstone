5a93f056537a0ebfe1286c0e7e07c6b31e5be054
EC2 Default User <ec2-user@ip-172-31-22-40.ap-northeast-1.compute.internal>
2017-09-04 04:54:11 +0000

    add new file

---
+++ janken/janken.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("janken.html")
+
+@app.route("/rock")
+def rock():
+    return render_template("rock.html",number = random.randint(1,3))
+
+@app.route("/paper")
+def paper():
+    return render_template("paper.html",number = random.randint(1,3))
+    
+@app.route("/scissors")
+def scissors():
+    return render_template("scissors.html",number = random.randint(1,3))
+    
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
+
+
